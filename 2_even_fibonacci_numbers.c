c76c651a8041a3434a83ed48d0f30feb0d32a7b1
qli <qli@DESKTOP-VVDRM16.localdomain>
2020-04-02 20:29:08 +0200

    added 2nd exercise

--- 2_even_fibonacci_numbers.c
+++ 2_even_fibonacci_numbers.c
@@ -7,4 +7,50 @@
 ** By considering the terms in the Fibonacci sequence 
 ** whose values do not exceed four million,
 ** find the sum of the even-valued terms.
-/*
+*/
+
+#include <stdio.h>
+
+/* VERSION 1 */
+
+// int main(void)
+// {
+// 	int limit = 4000000;
+// 	int sum = 0;
+// 	int a = 1;
+// 	int b = 1;
+// 	int value_hold = 0;
+
+// 	while (b < limit)
+// 	{
+// 		if (b % 2 == 0)
+// 			sum = sum + b;
+// 		value_hold = a + b;
+// 		a = b;
+// 		b = value_hold;
+// 	}
+// 	printf("sum is %d\n", sum);
+// 	return (0);
+// }
+
+/* VERSION 2 */
+/* to add up every third value */
+
+int main(void)
+{
+	int limit = 4000000;
+	int sum = 0;
+	int a = 1;
+	int b = 1;
+	int c = a + b;
+
+	while (b < limit)
+	{
+		sum = sum + c;
+		a = b + c;
+		b = c + a;
+		c = a + b;
+	}
+	printf("sum is %d\n", sum);
+	return (0);
+}
